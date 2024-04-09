c2c7a441e1c7f2bcc61cfa7088a896e38be25286
Harsh Gupta <harshguptaclj@gmail.com>
2024-04-09 16:25:28 +0530

    signin/signup form + header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="font-bold text-3xl">
-      Hare Krishna!
-    </div>
+    <Body/>
   );
 }
 
