bcb33a603539edb8f25949fa85793c017d8057ae
Harsh Gupta <harshguptaclj@gmail.com>
2024-04-14 13:28:14 +0530

    TMDB API data Fetch && Build Main Container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,55 @@
-import React from 'react'
-import { signOut } from "firebase/auth";
+import React, { useEffect } from 'react'
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from '../utils/firebase';
 import {useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
+import { addUser, removeUser } from '../utils/userSlice';
+import {useDispatch} from "react-redux"
+import { LOGO } from '../utils/constants';
 
 const Header = () => {
   const navigate =useNavigate();
+  const dispatch = useDispatch();
   const user= useSelector(store=> store.user);
   const handleSignOut=()=>{
     signOut(auth).then(() => {
-      navigate("/")
       // Sign-out successful.
     }).catch((error) => {
       // An error happened.
       navigate("/error");
     });
   }
+
+  useEffect(()=>{
+    const unsubscribe= onAuthStateChanged(auth , (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const {uid,email,displayName, photoURL} = user;
+        dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL: photoURL}));
+        navigate("/browse");
+      } 
+      else {
+        // User is signed out
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+
+    //Unsubscribe when components unmount
+    return ()=>unsubscribe();
+  },[])
+
   return (
     <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-20 flex justify-between">
       <img
         className="w-44"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={LOGO}
         alt="logo"
        />
        {user && <div className="flex p-2">
         <img
-          className="w-14 h-14 rounded-full my-2"
+          className="w-14 h-14 rounded-md my-2"
           alt="userIcon"
           src={user?.photoURL}
           ></img>
