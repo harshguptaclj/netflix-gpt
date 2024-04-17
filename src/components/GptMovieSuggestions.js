2717f778a76b7886ab50eb63792458e2a79be147
Harsh Gupta <harshguptaclj@gmail.com>
2024-04-17 12:00:19 +0530

    increased opacity

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -8,7 +8,7 @@ const GptMovieSuggestions = () => {
   if(!movieNames) return null;
 
   return (
-    <div className='p-4 m-4 bg-black text-white bg-opacity-50'>
+    <div className='p-4 m-4 bg-black text-white bg-opacity-70'>
       <div>
       {movieNames.map((movieName, index) =>(
         <MovieList  
